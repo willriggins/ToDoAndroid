0d10565b4d82e5e9970f69b1ff8436321c2924be
willriggins <williamriggins2013@gmail.com>
2016-06-01 10:00:01 -0400

    initial import

---
+++ app/src/main/java/theironyard/com/todoandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package theironyard.com.todoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
